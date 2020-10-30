241602225450dae5e3ed32700477c4bd77a4d073
Gabriel John Dusing <gabrieljdusing@gmail.com>
2020-10-29 20:20:25 -0400

    fixing empty state result

--- app/app.py
+++ app/app.py
@@ -130,17 +130,15 @@ def index():
 def search():
     try:
         term = request.form.get('keyword')
-    except:
-        return render_template("index.html", search_results="<p>No results found. Please check spelling or enter a different search term.</p>")
-        
-    if len(term) > 0 :
-        result = search_keyword(term)
-        if result is not None:
+        if len(term) > 0 :
+            result = search_keyword(term)
             result['is_disaster']=predict_disaster(result['body'])
             result=body_topic(result)
             display=result[['title', 'url', 'datePublished', 'is_disaster', 'predicted_topic']]
             return render_template("index.html", search_results=f"{display.to_html(render_links=True)}")
         else:
-            return render_template("index.html", search_results="<p>No results found. Please check spelling or enter a different search term.</p>")
-    else:
-        return render_template("index.html", search_results="<p>Please enter a search term.</p>")
+            return render_template("index.html", search_results="<p>Please enter a search term.</p>")
+    except:
+        return render_template("index.html", search_results="<p>No results found. Please check spelling or enter a different search term.</p>")
+
+    
